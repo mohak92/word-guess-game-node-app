cefe943a953bbe3b55696859364dc524e4e0bc64
Mohak Tamhane <tamhane.mohak@gmail.com>
2019-11-09 07:50:53 -0500

    Letter.js constructor implemented

--- Letter.js
+++ Letter.js
@@ -1,1 +1,26 @@
+class Letter {
+    constructor(letter) {
+        this.letter = letter; 
+        this.letterGuessed = false;
+        this.toString = function() {
+            if (this.letterGuessed) {
+                return this.letter;
+            }
+            else {
+                return "_";
+            }
+        };
+        this.checkLetter = function(ch) {
+            if (this.letter === ch) {
+                this.letterGuessed = true;
+                return true;
+            }
+            else {
+                return false;
+            }
+        };
 
+    }
+}
+
+module.exports = Letter;
