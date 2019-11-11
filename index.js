1e9efd4983b61bf8c8af625eb15989bc7d05b563
Mohak Tamhane <tamhane.mohak@gmail.com>
2019-11-11 14:58:18 -0500

    global variables created

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+var Word = require("./Word.js");
+var inquirer = require("inquirer");
+var chalk = require('chalk');
+var guessesRemaining;
+var wordToGuess;
 
+var jsReservedKeywords = ["const", "let", "var", "if", "else", "case", "break",
+    "void", "while", "with", "for", "default", "class", "constructor",
+    "import", "export", "module", "true", "false", "return",
+    "do", "typeof", "try", "catch", "finally", "undefined", "null", 
+    "extends", "implements", "interface"];
