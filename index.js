806f4ecf908170211ea6992f09277b12c37a5dae
Mohak Tamhane <tamhane.mohak@gmail.com>
2019-11-12 14:18:19 -0500

    Reset Game to initialize game

--- index.js
+++ index.js
@@ -12,7 +12,28 @@ var jsReservedKeywords = ["const", "let", "var", "if", "else", "case", "break",
 
 console.log(chalk.blue("Guess the reserved javascript keyword in 10 guesses or less.\n"));
 
+resetGame();
+
 function resetGame() {
+
+    inquirer.prompt([
+        {
+            type: "confirm",
+            name: "status",
+            message: "Play Game ?"
+        }
+    ]).then(function (response) {
+        if (response.status === true) {
+            wordToGuess = new Word();
+            wordToGuess.addWord(jsReservedKeywords[Math.floor(Math.random() * jsReservedKeywords.length)])
+            console.log("\n" + wordToGuess.displayWord() + "\n");
+            guessesRemaining = 10;
+            playGame();
+        }
+        else {
+            console.log(chalk.blue("\nThanks for playing!\n"));
+        }
+    });
 }
 
 function playGame() {
