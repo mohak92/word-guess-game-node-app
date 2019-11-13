9fd5773a19113988dd879d463838861ed5c0de1c
mohak92 <tamhane.mohak@gmail.com>
2019-11-13 09:09:37 -0500

    minor fixes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var wordToGuess;
 var jsReservedKeywords = ["const", "let", "var", "if", "else", "case", "break",
     "void", "while", "with", "for", "default", "class", "constructor",
     "import", "export", "module", "true", "false", "return",
-    "do", "typeof", "try", "catch", "finally", "undefined", "null", 
+    "do", "typeof", "try", "catch", "finally", "undefined", "null",
     "extends", "implements", "interface"];
 
 console.log(chalk.blue("Guess the reserved javascript keyword in 10 guesses or less.\n"));
