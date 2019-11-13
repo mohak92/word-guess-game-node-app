5ab732b2cbfbac8d6031c983370dabd798ba1ca0
Mohak Tamhane <tamhane.mohak@gmail.com>
2019-11-13 09:06:57 -0500

    rules and letter check word check implemented

--- index.js
+++ index.js
@@ -37,4 +37,41 @@ function resetGame() {
 }
 
 function playGame() {
+    if (!wordToGuess.wordFound()) {
+        if (guessesRemaining > 0) {
+            inquirer.prompt([
+                {
+                    type: "input",
+                    name: "letter",
+                    message: "Guess a letter!",
+                    validate: function (ch) {
+                        if (/^[A-Z]$/i.test(ch)) {
+                            return true;
+                        }
+                        return false;
+                    }
+                }
+            ])
+                .then(function (response) {
+                    if (wordToGuess.checkLetter(response.letter.toLowerCase())) {
+                        console.log(chalk.green("\nCorrect!!!\n"));
+                    }
+                    else {
+                        guessesRemaining--
+                        console.log(chalk.red(`\nIncorrect!!!\n`));
+                        console.log("Remaining Guesses: " + guessesRemaining + "\n");
+                    }
+                    console.log(wordToGuess.displayWord() + "\n");
+                    playGame();
+                });
+        }
+        else {
+            console.log(chalk.red("You ran out of guesses. Too bad, you lose!\n"));
+            resetGame();
+        }
+    }
+    else {
+        console.log(chalk.green("Congratulations, you got it right!\n"));
+        resetGame();
+    }
 }
